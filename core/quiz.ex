e946a5b985ca7778fda37872622028569a5e25a9
José Victor Zeller <josevictorzg@gmail.com>
2023-01-31 21:46:12 -0300

    chore: improve models descriptions

--- core/quiz.ex
+++ core/quiz.ex
@@ -1,5 +1,23 @@
 defmodule Mastery.Core.Quiz do
   @moduledoc """
   Represents the structure we need to as questions (quizzes) to users.
+
+  Initially, all questions will start in `templates`. The quiz will select a question,
+  and that question will move from `templates` to `used`. After all questions get
+  asked once, unless they're mastered in the meantime, they'll move back from `used` to
+  `templates`.
+
+  Getting an answer right will increment a record, and getting enough right in a row
+  will move a template from `used` to `mastered`. Getting an answer wrong will reset the
+  record.
   """
+
+  defstruct title: nil,
+            mastery: 3,
+            templates: %{},
+            used: [],
+            urrent_question: nil,
+            last_response: nil,
+            record: %{},
+            mastered: []
 end
