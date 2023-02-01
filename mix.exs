56be0451bbd267c4b1386b2fd3b4ddb7326ce5eb
José Victor Zeller <josevictorzg@gmail.com>
2023-01-31 22:02:27 -0300

    chore: add dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,19 @@ defmodule Mastery.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_deps: :apps_direct,
+        plt_add_apps: [
+          :ex_unit,
+          :mix
+        ],
+        list_unused_filters: true,
+        # we use the following opt to change the PLT path
+        # even though the opt is marked as deprecated, this is the doc-recommended way
+        # to do this
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+      ]
     ]
   end
 
@@ -22,7 +34,9 @@ defmodule Mastery.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false}
+      # Linters, formatters and typespecs
+      {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 end
